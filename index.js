1728d61e39a80dd57b71b336d99d5e369054b1ed
Rajath-55 <f20190122@goa.bits-pilani.ac.in>
2020-06-25 11:17:06 +0530

    Try

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const app = require('./app');
 
 const server = http.createServer(app);
 
-
+httpProxy.createProxyServer({
+    target: 'https://mernblograjath.herokuapp.com',
+    toProxy: true,
+    changeOrigin: true,
+    xfwd: true
+});
 
 
 server.listen(PORT);
